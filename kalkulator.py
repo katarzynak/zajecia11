8bb9ececc862e3f8d7ff593f0926bd86165cd728
katarzynak <eneny.askin@gmail.com>
2014-05-18 17:16:01 +0200

    dodanie drugiej funkcji dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -5,3 +5,9 @@ def dodawanie():
     print (a+b)
 
 dodawanie()
+
+def dodawanie(a,b):
+    return a+b
+l1 = int(input('Wprowadz pierwsza liczbe'))
+l2 = int(input('Wprowadz druga liczbe'))
+print(dodawanie(l1,l2))
